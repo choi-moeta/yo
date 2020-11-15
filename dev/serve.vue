4ed79dff358692bbfb8b4b23c6f821740c21e279
moeta <teqqmu@gmail.com>
2020-11-15 14:25:12 +0700

    Initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { Yo2Sample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    Yo2Sample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <yo2-sample />
+  </div>
+</template>
